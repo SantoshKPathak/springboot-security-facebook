73aca9bbbf861fcbbf1e18971a493c04edd2ff02
Santosh Pathak <pathaksa@us.ibm.com>
2020-08-11 11:06:13 -0400

    First commit

---
+++ src/main/java/com/santoshpathak/learnings/security/SpringbootSecurityFacebookApplication.java
@@ -0,0 +1,13 @@
+package com.santoshpathak.learnings.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityFacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityFacebookApplication.class, args);
+	}
+
+}
